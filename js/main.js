b2fc328a18030274cc6978a90ca96d6572fc063a
peggy0911 <peiru0911@email.com>
2021-01-15 16:55:43 +0800

    Remove gotStream

--- js/main.js
+++ js/main.js
@@ -131,7 +131,7 @@ function start() {
     audio: {deviceId: audioSource ? {exact: audioSource} : undefined},
     // video: {deviceId: videoSource ? {exact: videoSource} : undefined}
   };
-  navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(gotDevices).catch(handleError);
+  navigator.mediaDevices.getUserMedia(constraints).then(gotDevices).catch(handleError);
 }
 
 audioInputSelect.onchange = start;
